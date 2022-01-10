f5f83bab889029e1f372b7121c00dc5c0fab89aa
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-10 22:15:01 +0400

    ---------

---
+++ Lesson-2/Task-2/task2/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stifadəçi adından və soyadından müvafiq olaraq 2 giriş alan C# proqramı yazın
+            //və sonra onu konsolda "NAME SOYADI" (hər ikisi böyük hərf) kimi çap edin.
+            Console.WriteLine("ad:");
+            string ad = Console.ReadLine().ToUpper();
+            Console.WriteLine("Soyad:");
+            string soyad = Console.ReadLine().ToUpper();
+            Console.WriteLine(ad+" "+soyad);
+            Console.ReadKey();
+        }
+    }
+}
