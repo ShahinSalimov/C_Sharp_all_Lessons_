03177cec55557cb0f5de1f509362edbb4dddd241
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-10 22:05:55 +0400

    ----

---
+++ Lesson-2/Task-1/task1/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program that takes three letters as input and display them in reverse order.
+            //(Giriş kimi üç hərf qəbul edən C# proqramı yazın və onları tərs ardıcıllıqla göstərin.)
+            Console.WriteLine("soz daxil edin:");
+            char[] soz = Console.ReadLine().ToCharArray();
+            Array.Reverse(soz);
+            Console.WriteLine(soz);
+            Console.ReadKey();
+        }
+    }
+}
