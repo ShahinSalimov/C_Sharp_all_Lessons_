e4539057b30ea3eeb5fe9e157369711ff16b7dd8
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-11 21:28:06 +0400

    aaa

--- Lesson-2/Task-3/task3/task3/Program.cs
+++ Lesson-2/Task-3/task3/task3/Program.cs
@@ -10,6 +10,16 @@ namespace task3
     {
         static void Main(string[] args)
         {
+            //Dairənin radiusunu daxil edən C# proqramı yazın və çevrənin perimetrini
+            //və sahəsini hesablayın və onu konsolda çap edin.
+
+            Console.WriteLine("zehmet olmazsa dairenin radiusunu daxil edin:");
+            int radius = int.Parse(Console.ReadLine());
+            double pi=3.14;
+            double cevrenin_sahesi = pi * radius * radius;
+            double cevrenin_perimetri = 2 * pi * radius;
+            Console.WriteLine($"cevrenin perimetri {cevrenin_perimetri}-a beraberdir.\nCevrenin sahesi ise {cevrenin_sahesi}-a beraberdir.");
+            Console.ReadKey();
         }
     }
 }
