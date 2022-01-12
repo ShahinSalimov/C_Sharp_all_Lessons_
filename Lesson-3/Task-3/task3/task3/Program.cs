23ddf2ca46eb19bc26925dde7e08788c9ee1f2c7
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-12 21:49:59 +0400

    ----F

---
+++ Lesson-3/Task-3/task3/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İstifadəçidən 2 int nömrə girişi alın və dəqiq giriş1 / giriş2 hesablayın .
+            //Nümunə: 16, 5. Nəticə 3 yox, 3,2 olmalıdır.
+            Console.WriteLine("num1:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("num2:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+           double result=num1 / num2;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
