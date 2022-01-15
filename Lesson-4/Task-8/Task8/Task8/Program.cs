1ea4266fd2a7ccd9b197ac0086ac07aac7437b39
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-16 02:51:16 +0400

    ooooooooo

---
+++ Lesson-4/Task-8/Task8/Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İstənilən gün sayını tam ədədlə oxumaq
+            //və günün adını sözdə göstərmək üçün C# Sharp dilində proqram yazın.
+            //Test Məlumatı : 4 Gözlənilən Nəticə : Cümə axşamı
+
+            DateTime dd = DateTime.Now;
+            Console.WriteLine(dd.DayOfWeek.ToString());
+            Console.WriteLine(((int)dd.DayOfWeek)+1);
+            Console.ReadKey();
+
+
+        }
+    }
+}
