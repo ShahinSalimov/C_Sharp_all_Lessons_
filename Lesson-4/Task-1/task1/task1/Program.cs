ecef2986a5720a5a76b324f4852979521880a5f4
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-15 23:43:28 +0400

    -----

---
+++ Lesson-4/Task-1/task1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Doğum gününüzün həftə gününü (məs: Bazar ertəsi) hesablayan C# yazın
+            Console.WriteLine("zehmet olmazsa dogum tarixinizi qeyd edin:");
+            DateTime dt = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine(dt.DayOfWeek);
+            Console.ReadKey();
+        }
+    }
+}
