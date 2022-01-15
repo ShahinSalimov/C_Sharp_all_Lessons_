e2863d4cc70a3100033a1bd1e7e6d55ef764c87b
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-16 02:16:37 +0400

    iiiii

---
+++ Lesson-4/Task-4/task 4/task 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilən ilin, ayın və günün cari olub-olmadığını yoxlamaq üçün C# dilində proqram yazın.
+            Console.WriteLine("cari gunu daxil edin:");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("cari ayi daxil edin:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("cari ili daxil edin:");
+            int year = int.Parse(Console.ReadLine());
+            DateTime nd = DateTime.Now;
+            bool test = day == nd.Day && month == nd.Month && year == nd.Year;
+            Console.WriteLine($"daxil etdiyiniz tarix {day+"/"+month+"/"+year}");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("Netice :");
+            Console.WriteLine(test);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson-4/Task-5/task5/task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //daxil edilen tarixin son gununu cap eden c# programi yazin...
+            Console.WriteLine("day:");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("month:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("year:");
+            int year = int.Parse(Console.ReadLine());
+            DateTime nd = DateTime.Now;
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"daxil edilen tarix {day+"/"+month+"/"+year}");
+            Console.WriteLine("--------------------------------------------");
+            var find =31-nd.Day;
+            var lastDay = find + nd.Day;
+            Console.WriteLine($"daxil edilen tarixin son gunu: {lastDay+"/"+month+"/"+year}");
+            Console.ReadKey();
+        }
+    }
+}
