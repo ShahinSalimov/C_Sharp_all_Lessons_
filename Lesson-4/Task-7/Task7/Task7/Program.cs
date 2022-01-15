93e6a00e52fc00a8de4f459ec2f93ce07b7719bd
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-16 02:39:49 +0400

    ,.,.,.,.

---
+++ Lesson-4/Task-7/Task7/Task7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Üç ədədin ən böyüyünü tapmaq üçün C# Sharp proqramını yazın.
+            Console.WriteLine("num1:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("num2:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("num3:");
+            int num3 = int.Parse(Console.ReadLine());
+
+            if (num1>num2 && num2>num3)
+            {
+                Console.WriteLine($"{num1} ededi en boyuk ededdir.");
+            }
+            else if (num2 > num1 && num1 > num3)
+            {
+                Console.WriteLine($"{num2} ededi en boyuk ededdir.");
+            }
+            else
+            {
+                Console.WriteLine($"{num3} ededi en boyuk ededdir.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
