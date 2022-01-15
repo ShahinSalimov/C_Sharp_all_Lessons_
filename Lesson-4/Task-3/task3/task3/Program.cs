e0c2de9f46fa9979e7e4c37c7b1968f5ac1d2bb5
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-16 00:58:40 +0400

    **********

---
+++ Lesson-4/Task-3/task3/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bu andan 40 gün sonra həftənin hansı gününün olduğunu hesablamaq üçün C# proqramı yazın
+            DateTime nd = DateTime.Now;
+            
+            
+            Console.WriteLine($"cari tarixden 40gun sonra heftenin {nd.Add(TimeSpan.Parse("39.23:59:59")).DayOfWeek} gunudur....");
+            Console.ReadKey();
+        }
+    }
+}
