7204811c61b709db9689e23fe77b899f5c810174
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-16 22:12:23 +0400

    ---------------------

---
+++ Lesson-5/Task-5/Task5/Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program in C# to display the pattern like right angle triangle using an asterisk.
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
