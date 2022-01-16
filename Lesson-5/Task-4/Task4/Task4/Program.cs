6084b07e22b0891255e79066d0f2e062d3d77cb8
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-16 20:19:11 +0400

    -----------------------------

---
+++ Lesson-5/Task-4/Task4/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vurma cədvəlini şaquli olaraq 1-dən n -ə qədər göstərmək üçün C# dilində proqram yazın .
+            Console.WriteLine("vurma cedvelini istediyiniz sayda qeyd edin: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=n; i++)
+            {
+               
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i}x{j}={i * j}");
+                  
+                }
+                
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
