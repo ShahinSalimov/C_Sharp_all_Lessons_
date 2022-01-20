ed46288e2c513a75dcc44920b9e60c2e9644e344
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-20 21:47:28 +0400

    ----

---
+++ Lesson-6/Task4/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş ədədin ayrı-ayrı rəqəmlərinin cəmini hesablamaq üçün
+            //funksiya yaratmaq üçün C# dilində proqram yazın.
+           
+            Console.WriteLine(sum(3));
+            Console.ReadKey();
+        }
+
+        static int sum(int num)
+        {
+            int sum = 0;
+            for (int i = 1; i <=num; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
