81c947ac08295cbb89e5907fe725a680c66942f2
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-20 20:53:10 +0400

    ++++++

---
+++ Lesson-6/Task1/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             10 və 100 arasında dəyəri daxil edin. 
+            İstifadəçinin daxil etdiyi dəyərin uzunluğu ilə 
+            təsadüfi qiymətlərlə int massivini işə salın. 
+            Aşağıdakı tapşırıqları yerinə yetirin
+
+            1.C# sort massivində həm azalan , həm də artan qaydada proqram yazın
+            2.Massivin 3-cü maksimum elementini tapmaq üçün C# proqramı yazın.
+            3.İlk 4 maksimum elementi tapmaq üçün C# dilində proqram yazın.*/
+
+            Console.WriteLine("zehmet olmazsa 10-100 arasi bir reqem daxil edin :");
+            int num = int.Parse(Console.ReadLine());
+            int[] arr = new int[num];
+            Random rn = new Random();
+            for (int i=0; i<num;i++)
+            {
+                arr[i] = rn.Next(1,101);
+                Console.WriteLine($"x[{i}]={arr[i]}");
+            }
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("azdan coxa dogru..");
+            Array.Sort(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
