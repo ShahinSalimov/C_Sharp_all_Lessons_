8b8300103478ca5a2a61b734d9c36a5b90a00ca1
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-21 19:52:34 +0400

    8888

---
+++ Lesson-6/Task5/Task5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilmiş ədədin ayrı-ayrı rəqəmlərinin cəmini hesablamaq
+            //üçün funksiya yaratmaq üçün C# dilində proqram yazın. 
+            //Rəqəm daxil edin: 1234 Gözlənilən Nəticə : 1234 rəqəminin rəqəmlərinin cəmi : 10-dur
+            Console.WriteLine("number:");
+            char[] arr = Console.ReadLine().ToCharArray();
+            Console.WriteLine(FindSumNumber(arr));
+            Console.ReadKey();
+        }
+
+
+        static int FindSumNumber(char[] a)
+        {
+            int sum = 0;
+            foreach (var item in a)
+            {
+
+                string b = Convert.ToString(item);
+                int c = Convert.ToInt32(b);
+                sum += c;
+            }
+            return sum;
+        }
+    }
+}
