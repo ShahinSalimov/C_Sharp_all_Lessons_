8a40ab0dea6edb6c85a68b985290322426fa1946
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-23 12:52:37 +0400

    kkkk

---
+++ Lesson-7/Task4(hackerrrank)/Task4(hackerrrank)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_hackerrrank_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=4; i++)
+            {
+                for (int j = i; j < 4; j++)
+                {
+                    Console.Write (" ");
+                }
+                for (int k = 0; k <=i; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
