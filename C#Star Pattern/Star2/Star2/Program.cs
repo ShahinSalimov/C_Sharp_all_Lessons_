914283816ba07d3ebbec5f0981bf326bb97b41a4
ShahinSalimov <mrsalimofff@gmail.com>
2022-01-23 20:04:52 +0400

    aaa

---
+++ C#Star Pattern/Star2/Star2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Star2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 0; j <i; j++) //*****
+                {
+                    Console.Write("* ");   //
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
