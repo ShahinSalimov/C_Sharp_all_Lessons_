7bd5e805dd37c705bd97638843e86f6b25885944
ShahinSalimov <mrsalimofff@gmail.com>
2022-03-01 20:31:13 +0400

    909090

---
+++ Lesson-9/Task3/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoBook book = new PhotoBook();
+            Console.WriteLine(book.GetNumberPages(233));
+            PhotoBook book2= new PhotoBook();
+            Console.WriteLine(book.GetNumberPages(24));
+            PhotoBook book3= new PhotoBook();
+            Console.WriteLine(book.GetNumberPages(600));
+        }
+    }
+    public class PhotoBook
+    {
+        public int pages { get; set; }
+        
+        public int GetNumberPages(int page)
+        {
+            pages = page;
+            return pages;
+        }
+    }
+}
